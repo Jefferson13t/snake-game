2bbcad988635f3b16736a32ceea5cd90ea92d0fa
Jefferson13t <jefersonpereira1331@gmail.com>
2020-07-16 14:53:50 -0300

    snake game completed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # snake-game
 recreating the snake game
 
-trying to make a copy of the snake game with HTML, CSS and JavaScript
+A snake game made from a project of Digital inovation one with html, Css and javascript.
--- js/script.js
+++ js/script.js
@@ -6,8 +6,13 @@ snake[0] = {
     x: 2 * box,
     y: 6 * box
 }
-//inicia a cobrinha sempre indo pra direita
+
+//inicia a cobrinha sempre indo pra direita e a cobrinha 2 sempre indo pra baixo
 let direction = "right";
+
+//pega o elemento pontuação
+let pt = document.getElementById("pt")
+let t = 0
 
 //cria valores aleatorios para usar na posicao da comida
 let food = {
@@ -21,20 +26,34 @@ function criarBG() {
     context.fillRect(0, 0, 16 * box, 16 * box);
 
 }
+
 //cria a cobrinha
 function criarcobrinha() {
     for(i = 0; i < snake.length; i++) {
-        context.fillStyle = "green";
-        context. fillRect(snake[i].x, snake[i].y, box, box);
+
+//muda a cor do corpo da cobrinha em relação à cabeça
+        if(snake[i].x == snake[0].x && snake[i].y == snake[0].y){
+            context.fillStyle = "#4B3970";
+
+        } else {
+            context.fillStyle = "#8164BD";
+
+        }
+
+        context.fillRect(snake[i].x, snake[i].y, box, box);
        
     }
 
 }
 
-//desenha a comida em algum lugar aleatorio
+//desenha a comida em algum lugar aleatorio 
 function drawfood() {
-    context.fillStyle = "red";
+    for (i = 0; i < snake.length; i++){
+
+    context.fillStyle = "#32703C";
     context.fillRect(food.x, food.y, box, box);
+
+}
 }
 
 
@@ -42,24 +61,31 @@ function drawfood() {
 document.addEventListener('keydown', update);
 
 function update (event) {
-    if (event.keyCode == 37 && direction != "right") direction = "left";
-    if (event.keyCode == 38 && direction != "up") direction = "down"; 
-    if (event.keyCode == 39 && direction != "left") direction = "right";
-    if (event.keyCode == 40 && direction != "down") direction = "up"; 
-
-    
-
-
+    if (event.keyCode == 37 && direction != "right" || event.keyCode == 65 && direction != "right" ) direction = "left";
+    if (event.keyCode == 38 && direction != "up"    || event.keyCode == 87 && direction != "up") direction = "down"; 
+    if (event.keyCode == 39 && direction != "left"  || event.keyCode == 68 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "down"  || event.keyCode == 83 && direction != "down") direction = "up"; 
 } 
 
 //inicia o jogo
 function iniciarjogo() {
 
 //faz a cobrinha mudar atravessar a parede de um lado e sair do outro
-    if(snake[0].x > 14 * box && direction =="right") snake[0].x = 0; 
-    if(snake[0].x < 1 && direction =="left") snake[0].x = 16 * box;
-    if(snake[0].y > 14 * box && direction =="up") snake[0].y = 0; 
-    if(snake[0].y < 1 && direction =="down") snake[0].y = 15 * box;  
+    if(snake[0].x > 15 * box && direction =="right") snake[0].x = 0; 
+    if(snake[0].x < 0 && direction =="left") snake[0].x = 15 * box;
+    if(snake[0].y > 15 * box && direction =="up") snake[0].y = 0; 
+    if(snake[0].y < 0 && direction =="down") snake[0].y = 15 * box; 
+    
+
+//tela de game over
+    for(i = 1; i< snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            window.alert("GAME OVER!! Sua pontuação foi de: " + t  + " pontos.");
+            window.alert("recarregue a página para recomeçar")
+        }
+
+    }
 
 //chama as funcoes
     criarBG();
@@ -69,30 +95,36 @@ function iniciarjogo() {
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
+
 // move a cobrinha
-    if (direction == "right") snakeX += box;
+    if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
-    if (direction == "up") snakeY += box;
+    if(direction == "up") snakeY += box;
     if(direction == "down") snakeY -= box;
+
 
 //remove o ultimo valor do array da cobrinha exceto quando ela esta comendo a comida 
 //remove a comida e adiciona outra em algum lugar
-
+//aumenta a pontuação
 if (snake[0].x == food.x && snake[0].y == food.y)  {
-        food.x = Math.floor(Math.random() * 15 + 1) * box,
-        food.y = Math.floor(Math.random() * 15 + 1) * box
-} else { snake.pop() }
+    food.x = Math.floor(Math.random() * 15 + 1) * box,
+    food.y = Math.floor(Math.random() * 15 + 1) * box
+    
+    t = t + 100
+    pt.innerText = t
+
+} else {snake.pop()}
 
 
 
-    let newhead = {
-        x: snakeX,
-        y: snakeY
-    }
+let newhead = {
+    x: snakeX,
+    y: snakeY
+}
 
-    snake.unshift(newhead);
+snake.unshift(newhead);
+
 
 }
+
 let jogo = setInterval(iniciarjogo, 100);
-
-
